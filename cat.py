1bc0c1747b6e7003857d6e583d5fcc74957d4a65
SecurityGuyJW <35028309+SecurityGuyJW@users.noreply.github.com>
2018-01-08 07:27:33 -0800

    Add files via upload

---
+++ cat.py
@@ -0,0 +1,17 @@
+#************************************************************************************
+#*									  	    *
+#*		Code below show cases cat function via SH repository		    *
+#*										    *
+#*										    *
+#************************************************************************************
+
+
+import sh
+
+#the cat function is piped into the var variable.
+var = sh.cat('index.txt')
+
+#output of the text file is displayed.
+print(var)
+
+
